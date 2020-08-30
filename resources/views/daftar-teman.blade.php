76cf32495512a18c2f44aa054be62c47e0fff913
harisanwar129 <harisanwar129@gmail.com>
2020-08-30 21:20:11 +0700

    routing

---
+++ resources/views/daftar-teman.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Daftar Teman Haris</title>
+</head>
+<body>
+        <h3>Daftar Teman Haris Anwar</h3>
+</body>
+</html>
