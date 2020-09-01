b3c52589a9956b738e00b58844804c4f79a8975d
harisanwar129 <harisanwar129@gmail.com>
2020-09-01 12:28:28 +0700

    mengubah tabel yang sudah ada

---
+++ database/migrations/2020_09_01_020913_create_table_category.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',60);
+            $table->string('keywords',40);
+            $table->string('description',155);
+            $table->string('img',40);
+            $table->string('icon',40);
+            $table->enum('publish',['0','1']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorys');
+    }
+}
