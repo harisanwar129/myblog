3cd7c81399f6d8637c5c127879ff57d1a0d968ac
harisanwar129 <harisanwar129@gmail.com>
2020-09-05 10:34:37 +0700

    cleared

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users','profilsController@index');
-Route::get('/add','profilsController@add');
-Route::get('/update','profilsController@update');
-Route::get('/delete','profilsController@delete');
-Route::get('/postsbyusers/{id}','profilsController@posts');
-Route::get('/allposts','postsController@index');
-Route::get('/addpost','postsController@add');
-Route::get('/updatepost','postsController@update');
-Route::get('/deletepost','postsController@delete');
-Route::get('/postbycategory','postsController@postbycategory');
-Route::get('/categorypost','postsController@categorypost');
-Route::get('/insertpostscategories','postsController@insertpostscategories');
